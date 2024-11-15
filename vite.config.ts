14f95d74e1cde94f99b4c05b9dcb86593a36daa2
belalkaram <karambelal91@gmail.com>
2024-11-15 16:20:34 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.js',
+          dest: 'assets'
+        }
+      ]
+    })
+  ],
+  optimizeDeps: {
+    exclude: ['lucide-react']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          pdfjs: ['pdfjs-dist']
+        }
+      }
+    }
+  }
+});
